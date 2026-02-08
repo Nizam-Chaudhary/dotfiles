4013de2a566c0506da50d1c77f497ce426ceb048
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2026-02-08 23:47:03 +0530

    feat: added pomodoro functions

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -84,3 +84,26 @@ img2png() {
     -define png:exclude-chunk=all \
     "${img%.*}-optimized.png"
 }
+
+# study stream aliases
+# Requires https://github.com/caarlos0/timer to be installed. spd-say should ship with your distro
+
+declare -A pomo_options
+pomo_options["work"]="50"
+pomo_options["short-work"]="25"
+pomo_options["break"]="10"
+pomo_options["short-break"]="5"
+
+pomodoro () {
+  if [ -n "$1" -a -n "${pomo_options["$1"]}" ]; then
+  val=$1
+  echo $val | lolcat
+  timer ${pomo_options["$val"]}m
+  # spd-say "'$val' session done"
+  fi
+}
+
+alias wo="pomodoro 'work'"
+alias br="pomodoro 'break'"
+alias swo="pomodoro 'short-work'"
+alias sbr="pomodoro 'short-break'"
